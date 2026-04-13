e789467c8d3b7b74aa2854bef4bb8907a4d5e8ef
Markleprimes <102720819+Markleprimes@users.noreply.github.com>
2026-04-13 21:33:14 +0700

    Update netrunner.js

--- netrunner.js
+++ netrunner.js
@@ -2,7 +2,7 @@
 
 const NR_CONFIG = {
   easy: { size: 8, extraOpenings: 8, timeLimit: 10 },
-  medium: { size: 12, extraOpenings: 7, timeLimit: 10 },
+  medium: { size: 12, extraOpenings: 7, timeLimit: 10, maxOpen: 220},
   hard: { size: 16, extraOpenings: 2, timeLimit: 10, stairPatterns: 4, minPath: 40, maxOpen: 118 }
 };
 
